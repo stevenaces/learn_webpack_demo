9aa5912247fddbc5c75cee01649788f7881283cc
stevenaces <3313899200@qq.com>
2023-01-11 21:26:03 +0800

    update: 重新组织readme和note文件，注释了webpack等其他一些文件

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-	plugins: [
-		// require("autoprefixer"),
-		require("postcss-preset-env"), // 也可以向下面这样，直接使用字符串
-		// "postcss-preset-env",
-	],
-};
+// module.exports = {
+// 	plugins: [
+// 		// require("autoprefixer"),
+// 		require("postcss-preset-env"), // 也可以向下面这样，直接使用字符串
+// 		// "postcss-preset-env",
+// 	],
+// };
